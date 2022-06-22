faf5f6b5cabd015f23008a56f57646920e5701cb
lightY <lighty8214@gmail.com>
2022-06-22 18:08:52 +0430

    Required contracts were imported for LaqiraceCollectibles contract

--- contracts/LaqiraceCollectibles.sol
+++ contracts/LaqiraceCollectibles.sol
@@ -1,6 +1,11 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract LaqiraceCollectibles {
-    
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract LaqiraceCollectibles is ERC721Enumerable, Ownable {
+
+    constructor() ERC721("LaqiraceNFT", "LRNFT") {}
 }
